072ca67612cebe859723336f0d733e7af280d82d
gonzalo <gonzalojpv@gmail.com>
2023-01-21 00:14:42 -0600

    GP, Add product

---
+++ src/components/ProductCard/index.vue
@@ -0,0 +1,116 @@
+<script setup lang="ts">
+interface Props {
+  name: string
+  price: number
+  photo: string
+  description: string
+  priceWithoutTax: number
+  priceWithTax: number
+  priceWithoutTaxUnitary: number
+  priceWithTaxUnitary: number
+  pricePublic: number
+  pricePublicUnitary: number
+}
+
+withDefaults(defineProps<Props>(), {
+  name: 'Product Name',
+  price: 0,
+  photo: 'https://via.placeholder.com/192x288.png',
+  description: ''
+})
+
+const setAltImg = (event) => {
+  event.target.src = 'https://via.placeholder.com/192x288.png'
+}
+</script>
+<template>
+  <div class="bg-white">
+    <div class="pt-6 pb-16 sm:pb-24">
+      <div class="mx-auto mt-8 max-w-2xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+        <div class="lg:grid lg:auto-rows-min lg:grid-cols-2 lg:gap-x-2">
+          <h1 class="col-span-2 mb-3 text-xl font-medium text-gray-900">
+            {{ name }}
+          </h1>
+          <!-- Image gallery -->
+          <div class="mt-8 lg:mt-0">
+            <img
+              :src="photo"
+              alt="New Product"
+              class="rounded-lg"
+              @error="setAltImg"
+            />
+          </div>
+          <div>
+            <div class="flex">
+              <span
+                class="ml-2 rounded-full bg-green-600 py-0.5 px-2 text-xl font-medium tracking-wide text-gray-50"
+                >${{ price }}</span
+              >
+            </div>
+            <div class="mt-10">
+              <h2 class="text-sm font-medium text-gray-900">Description</h2>
+
+              <div class="prose prose-sm mt-4 text-gray-500">
+                <p v-html="description"></p>
+              </div>
+            </div>
+          </div>
+          <div class="col-span-2 mt-8 flex flex-col">
+            <div>
+              <dl class="mt-2 space-y-6 text-sm font-medium text-gray-500">
+                <div v-if="pricePublicUnitary" class="flex justify-between">
+                  <dt>Unitary Price</dt>
+                  <dd class="text-gray-900">${{ pricePublicUnitary }}</dd>
+                </div>
+                <div v-if="pricePublic" class="flex justify-between">
+                  <dt>Public Price</dt>
+                  <dd class="text-gray-900">${{ pricePublic }}</dd>
+                </div>
+                <div v-if="priceWithoutTax" class="flex justify-between">
+                  <dt class="flex">
+                    Price
+                    <span
+                      class="ml-2 rounded-full bg-red-600 py-0.5 px-2 text-xs tracking-wide text-gray-50"
+                      >Sin IVA</span
+                    >
+                  </dt>
+                  <dd class="text-gray-900">${{ priceWithoutTax }}</dd>
+                </div>
+                <div v-if="priceWithTax" class="flex justify-between">
+                  <dt class="flex">
+                    Price Con IVA
+                    <span
+                      class="ml-2 rounded-full bg-green-600 py-0.5 px-2 text-xs tracking-wide text-gray-50"
+                      >Con IVA</span
+                    >
+                  </dt>
+                  <dd class="text-gray-900">${{ priceWithTax }}</dd>
+                </div>
+                <div v-if="priceWithoutTaxUnitary" class="flex justify-between">
+                  <dt class="flex">
+                    Unitary Price
+                    <span
+                      class="ml-2 rounded-full bg-red-600 py-0.5 px-2 text-xs tracking-wide text-gray-50"
+                      >Sin IVA</span
+                    >
+                  </dt>
+                  <dd class="text-gray-900">${{ priceWithoutTaxUnitary }}</dd>
+                </div>
+                <div v-if="priceWithTaxUnitary" class="flex justify-between">
+                  <dt class="flex">
+                    Unitary Price
+                    <span
+                      class="ml-2 rounded-full bg-green-600 py-0.5 px-2 text-xs tracking-wide text-gray-50"
+                      >Con IVA</span
+                    >
+                  </dt>
+                  <dd class="text-gray-900">${{ priceWithTaxUnitary }}</dd>
+                </div>
+              </dl>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/pages/product/new.vue
+++ src/views/pages/product/new.vue
@@ -3,6 +3,7 @@ import useVuelidate from '@vuelidate/core'
 import useProduct from '@/composables/product'
 import router from '@/router'
 import BarcodeGenerator from '@/components/BarcodeGenerator/index.vue'
+import ProductCard from '@/components/ProductCard/index.vue'
 
 import type { Product as BaseProps } from '@/stores/models/product.model'
 import { reactive, computed, ref } from 'vue'
@@ -27,7 +28,8 @@ const form: Props = reactive({
   priceWithoutTaxUnitary: 0,
   priceWithTax: 0,
   priceWithoutTax: 0,
-  revenue: 0
+  revenue: 0,
+  photo: ''
 })
 
 const { showNotification } = useNotification()
@@ -123,287 +125,230 @@ document.addEventListener('keydown', (evt) => {
 </script>
 
 <template>
-  <form class="px-4 sm:px-6 lg:px-8" @submit.prevent="onSubmit">
-    <div class="mt-10 pt-10">
-      <h2 class="text-lg font-medium text-gray-900">Product information</h2>
-      <div class="mt-4 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-4">
-        <!-- Name -->
-        <div>
-          <label for="first-name" class="form-label">Name</label>
-          <div class="mt-1">
-            <input
-              id="name"
-              v-model="form.name"
-              type="text"
-              name="name"
-              autocomplete="name"
-              class="form-control"
-              :class="{ 'is-invalid': v$.name.$errors.length }"
-              @blur="v$.name.$touch()"
-            />
-            <p
-              v-for="err of v$.name.$errors"
-              v-show="v$.name.$error"
-              :key="err.$uid"
-              class="invalid-feedback"
-              role="alert"
-            >
-              {{ err.$message }}
-            </p>
-          </div>
-        </div>
-        <!-- /.Name -->
-        <div v-if="form.barCode" class="text-center">
-          <BarcodeGenerator :value="form.barCode" />
-        </div>
-        <!-- quantity -->
-        <div>
-          <label for="quantity" class="form-label">Quantity</label>
-          <div class="mt-1">
-            <input
-              id="quantity"
-              v-model="form.quantity"
-              type="number"
-              name="quantity"
-              autocomplete="quantity"
-              class="form-control"
-              :class="{ 'is-invalid': v$.quantity.$errors.length }"
-              @blur="v$.quantity.$touch()"
-              @input="handlePrices"
-            />
-            <p
-              v-for="err of v$.quantity.$errors"
-              v-show="v$.quantity.$error"
-              :key="err.$uid"
-              class="invalid-feedback"
-              role="alert"
-            >
-              {{ err.$message }}
-            </p>
-          </div>
-        </div>
-        <!-- /.quantity -->
-        <!-- priceWithoutTax -->
-        <div>
-          <label for="last-name" class="form-label">Price without Tax</label>
-          <div class="mt-1">
-            <input
-              id="price"
-              v-model="form.priceWithoutTax"
-              type="text"
-              name="price"
-              autocomplete="price"
-              class="form-control"
-              :class="{ 'is-invalid': v$.priceWithoutTax.$errors.length }"
-              @blur="v$.priceWithoutTax.$touch()"
-              @input="handlePrices"
-            />
-            <p
-              v-for="err of v$.priceWithoutTax.$errors"
-              v-show="v$.priceWithoutTax.$error"
-              :key="err.$uid"
-              class="invalid-feedback"
-              role="alert"
-            >
-              {{ err.$message }}
-            </p>
-          </div>
-        </div>
-        <!-- /.priceWithoutTax -->
-        <!-- priceWithTax -->
-        <div>
-          <label for="unitPrice" class="form-label">priceWithTax</label>
-          <div class="mt-1">
-            <input
-              id="unitPrice"
-              v-model="form.priceWithTax"
-              type="number"
-              name="unitPrice"
-              min="0"
-              step="0.1"
-              autocomplete="unit-price"
-              class="form-control"
-              readonly
-            />
-          </div>
-        </div>
-        <!-- /.priceWithTax -->
-        <!-- priceWithoutTaxUnitary -->
-        <div>
-          <label for="unitPrice" class="form-label"
-            >priceWithoutTaxUnitary</label
-          >
-          <div class="mt-1">
-            <input
-              id="unitPrice"
-              v-model="form.priceWithoutTaxUnitary"
-              type="number"
-              name="unitPrice"
-              min="0"
-              step="0.1"
-              autocomplete="unit-price"
-              class="form-control"
-              readonly
-            />
-          </div>
-        </div>
-        <!-- /.priceWithoutTaxUnitary -->
-        <!-- priceWithTaxUnitary -->
-        <div>
-          <label for="price" class="form-label">priceWithTaxUnitary</label>
-          <div class="mt-1">
-            <input
-              id="price"
-              v-model="form.priceWithTaxUnitary"
-              type="text"
-              name="price"
-              autocomplete="price"
-              class="form-control"
-              readonly
-            />
-          </div>
-        </div>
-        <!-- /.priceWithTaxUnitary -->
-        <!-- pricePublic -->
-        <div>
-          <label for="price" class="form-label">Public price</label>
-          <div class="mt-1">
-            <input
-              id="price"
-              v-model="form.pricePublic"
-              type="text"
-              name="price"
-              autocomplete="price"
-              class="form-control"
-              :class="{ 'is-invalid': v$.pricePublic.$errors.length }"
-              @blur="v$.pricePublic.$touch()"
-            />
-            <p
-              v-for="err of v$.pricePublic.$errors"
-              v-show="v$.pricePublic.$error"
-              :key="err.$uid"
-              class="invalid-feedback"
-              role="alert"
-            >
-              {{ err.$message }}
-            </p>
-          </div>
-        </div>
-        <!-- /.pricePublic -->
-        <!-- pricePublicUnitary -->
-        <div>
-          <label for="price" class="form-label">pricePublicUnitary</label>
-          <div class="mt-1">
-            <input
-              id="price"
-              v-model="form.pricePublicUnitary"
-              type="text"
-              name="price"
-              autocomplete="price"
-              class="form-control"
-              readonly
-            />
-          </div>
-        </div>
-        <!-- /.pricePublic -->
-        <!-- revenue -->
-        <div>
-          <label for="price" class="form-label">revenue</label>
-          <div class="mt-1">
-            <input
-              id="price"
-              v-model="form.revenue"
-              type="text"
-              name="price"
-              autocomplete="price"
-              class="form-control"
-              readonly
-            />
-          </div>
-        </div>
-        <!-- /.revenue -->
-        <!-- price -->
-        <div>
-          <label for="price" class="form-label">price</label>
-          <div class="mt-1">
-            <input
-              id="price"
-              v-model="form.price"
-              type="text"
-              name="price"
-              autocomplete="price"
-              class="form-control"
-              readonly
-            />
-          </div>
-        </div>
-        <!-- /.price -->
-        <!-- Code -->
-        <div>
-          <label for="code" class="form-label">Code</label>
-          <div class="mt-1">
-            <input
-              id="code"
-              v-model="form.code"
-              type="text"
-              name="code"
-              autocomplete="address-level1"
-              class="form-control"
-              :class="{ 'is-invalid': v$.code.$errors.length }"
-              @blur="v$.code.$touch()"
-            />
-          </div>
-        </div>
-        <!-- /.Code -->
-
-        <div>
-          <label for="bar-code" class="form-label">Bar code</label>
-          <div class="mt-1">
-            <input
-              id="bar-code"
-              v-model="form.barCode"
-              type="text"
-              name="bar-code"
-              readonly
-              autocomplete="bar-code"
-              class="form-control"
-              :class="{ 'is-invalid': v$.barCode.$errors.length }"
-              @blur="v$.barCode.$touch()"
-            />
-          </div>
-        </div>
-        <!-- stock -->
-        <div>
-          <label for="stock" class="form-label">Stock</label>
-          <div class="mt-1">
-            <input
-              id="stock"
-              v-model="form.stock"
-              type="number"
-              name="stock"
-              autocomplete="stock"
-              class="form-control"
-              :class="{ 'is-invalid': v$.stock.$errors.length }"
-              @blur="v$.stock.$touch()"
-            />
-            <p
-              v-for="err of v$.stock.$errors"
-              v-show="v$.stock.$error"
-              :key="err.$uid"
-              class="invalid-feedback"
-              role="alert"
-            >
-              {{ err.$message }}
-            </p>
-          </div>
-        </div>
-        <!-- /.stock -->
-        <div class="text-right sm:col-span-2">
-          <button :disabled="v$.$invalid || toTry" type="submit" class="btn">
-            {{ buttonText }}
-          </button>
-        </div>
-      </div>
+  <div class="grid grid-cols-1 px-4 sm:grid-cols-2 sm:px-6 lg:px-8">
+    <div>
+      <ProductCard
+        :description="'Lorem ipsum elit, urna consequat felis vehicula mollis dictumst, aenean non a in donec nulla.'"
+        :name="form.name"
+        :price="Number(form.price)"
+        :photo="form.photo"
+        :price-without-tax="Number(form.priceWithoutTax)"
+        :price-with-tax="Number(form.priceWithTax)"
+        :price-without-tax-unitary="Number(form.priceWithoutTaxUnitary)"
+        :price-with-tax-unitary="Number(form.priceWithTaxUnitary)"
+        :price-public="Number(form.pricePublic)"
+        :price-public-unitary="Number(form.pricePublicUnitary)"
+      />
     </div>
-  </form>
+    <div>
+      <form class="px-4 sm:px-6 lg:px-8" @submit.prevent="onSubmit">
+        <div class="mt-10 pt-10">
+          <h2 class="text-lg font-medium text-gray-900">Product information</h2>
+          <div class="mt-4 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-4">
+            <!-- Name -->
+            <div>
+              <label for="first-name" class="form-label">Name</label>
+              <div class="mt-1">
+                <input
+                  id="name"
+                  v-model="form.name"
+                  type="text"
+                  name="name"
+                  autocomplete="name"
+                  class="form-control"
+                  :class="{ 'is-invalid': v$.name.$errors.length }"
+                  @blur="v$.name.$touch()"
+                />
+                <p
+                  v-for="err of v$.name.$errors"
+                  v-show="v$.name.$error"
+                  :key="err.$uid"
+                  class="invalid-feedback"
+                  role="alert"
+                >
+                  {{ err.$message }}
+                </p>
+              </div>
+            </div>
+            <!-- /.Name -->
+            <div v-if="form.barCode" class="text-center">
+              <BarcodeGenerator :value="form.barCode" />
+            </div>
+            <!-- quantity -->
+            <div>
+              <label for="quantity" class="form-label">Quantity</label>
+              <div class="mt-1">
+                <input
+                  id="quantity"
+                  v-model="form.quantity"
+                  type="number"
+                  name="quantity"
+                  autocomplete="quantity"
+                  class="form-control"
+                  :class="{ 'is-invalid': v$.quantity.$errors.length }"
+                  @blur="v$.quantity.$touch()"
+                  @input="handlePrices"
+                />
+                <p
+                  v-for="err of v$.quantity.$errors"
+                  v-show="v$.quantity.$error"
+                  :key="err.$uid"
+                  class="invalid-feedback"
+                  role="alert"
+                >
+                  {{ err.$message }}
+                </p>
+              </div>
+            </div>
+            <!-- /.quantity -->
+            <!-- priceWithoutTax -->
+            <div>
+              <label for="last-name" class="form-label"
+                >Price without Tax</label
+              >
+              <div class="mt-1">
+                <input
+                  id="price"
+                  v-model="form.priceWithoutTax"
+                  type="text"
+                  name="price"
+                  autocomplete="price"
+                  class="form-control"
+                  :class="{ 'is-invalid': v$.priceWithoutTax.$errors.length }"
+                  @blur="v$.priceWithoutTax.$touch()"
+                  @input="handlePrices"
+                />
+                <p
+                  v-for="err of v$.priceWithoutTax.$errors"
+                  v-show="v$.priceWithoutTax.$error"
+                  :key="err.$uid"
+                  class="invalid-feedback"
+                  role="alert"
+                >
+                  {{ err.$message }}
+                </p>
+              </div>
+            </div>
+            <!-- /.priceWithoutTax -->
+            <!-- revenue -->
+            <div>
+              <label for="price" class="form-label">revenue</label>
+              <div class="mt-1">
+                <input
+                  id="price"
+                  v-model="form.revenue"
+                  type="text"
+                  name="price"
+                  autocomplete="price"
+                  class="form-control"
+                  readonly
+                />
+              </div>
+            </div>
+            <!-- /.revenue -->
+            <!-- price -->
+            <div>
+              <label for="price" class="form-label">price</label>
+              <div class="mt-1">
+                <input
+                  id="price"
+                  v-model="form.price"
+                  type="text"
+                  name="price"
+                  autocomplete="price"
+                  class="form-control"
+                  readonly
+                />
+              </div>
+            </div>
+            <!-- /.price -->
+            <!-- Code -->
+            <div>
+              <label for="code" class="form-label">Code</label>
+              <div class="mt-1">
+                <input
+                  id="code"
+                  v-model="form.code"
+                  type="text"
+                  name="code"
+                  autocomplete="address-level1"
+                  class="form-control"
+                  :class="{ 'is-invalid': v$.code.$errors.length }"
+                  @blur="v$.code.$touch()"
+                />
+              </div>
+            </div>
+            <!-- /.Code -->
+
+            <div>
+              <label for="bar-code" class="form-label">Bar code</label>
+              <div class="mt-1">
+                <input
+                  id="bar-code"
+                  v-model="form.barCode"
+                  type="text"
+                  name="bar-code"
+                  readonly
+                  autocomplete="bar-code"
+                  class="form-control"
+                  :class="{ 'is-invalid': v$.barCode.$errors.length }"
+                  @blur="v$.barCode.$touch()"
+                />
+              </div>
+            </div>
+            <!-- stock -->
+            <div>
+              <label for="stock" class="form-label">Stock</label>
+              <div class="mt-1">
+                <input
+                  id="stock"
+                  v-model="form.stock"
+                  type="number"
+                  name="stock"
+                  autocomplete="stock"
+                  class="form-control"
+                  :class="{ 'is-invalid': v$.stock.$errors.length }"
+                  @blur="v$.stock.$touch()"
+                />
+                <p
+                  v-for="err of v$.stock.$errors"
+                  v-show="v$.stock.$error"
+                  :key="err.$uid"
+                  class="invalid-feedback"
+                  role="alert"
+                >
+                  {{ err.$message }}
+                </p>
+              </div>
+            </div>
+            <!-- /.stock -->
+            <!-- photo -->
+            <div>
+              <label for="stock" class="form-label">Photo</label>
+              <div class="mt-1">
+                <input
+                  id="photo"
+                  v-model="form.photo"
+                  type="url"
+                  name="photo"
+                  autocomplete="photo"
+                  class="form-control"
+                />
+              </div>
+            </div>
+            <!-- /.photo -->
+            <div class="text-right sm:col-span-2">
+              <button
+                :disabled="v$.$invalid || toTry"
+                type="submit"
+                class="btn"
+              >
+                {{ buttonText }}
+              </button>
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
 </template>
